940df51f716a568706d355c1f4a4e53adfdd0ea1
CaioLTG <caiolucas.tg@proton.me>
2025-08-26 19:45:50 -0300

    Import jakarta.persistence que estavam faltando

--- todolist/src/main/java/listadetarefas/todolist/entity/Tarefa.java
+++ todolist/src/main/java/listadetarefas/todolist/entity/Tarefa.java
@@ -1,6 +1,10 @@
 package listadetarefas.todolist.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tarefas")
